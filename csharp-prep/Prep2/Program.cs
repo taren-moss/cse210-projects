a6038fe2a7f2ca30e3677a878ac3f393ef807f2f
taren-moss <tarendmoss@gmail.com>
2023-04-19 17:32:25 -0700

    Finished Prep 1 and Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your Grade Percentage? ");
+        string userInput = Console.ReadLine();
+        int numberInput = int.Parse(userInput);
+
+        if (numberInput >= 90)
+        {
+            Console.WriteLine("Your Letter Grade is 'A'.");
+        }
+        else if (numberInput >= 80)
+        {
+            Console.WriteLine("Your Letter Grade is 'B'.");
+        }
+        else if (numberInput >= 70)
+        {
+            Console.WriteLine("Your Letter Grade is 'C'.");
+        }
+        else if (numberInput >= 60)
+        {
+            Console.WriteLine("Your Letter Grade is 'D'.");
+        }
+        else
+        {
+            Console.WriteLine("Your Letter Grade if 'F'.");
+        }
+
+        if (numberInput >= 70)
+        {
+            Console.Write("Congratulations! You passed the class!");
+        }
+        else
+        {
+            Console.Write("You failed the class.. don't worry, you'll do better next time!");
+        }
     }
 }
